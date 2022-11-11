6042ba7498337b893d5af38464ef1d43b3e76c15
Lukasz Rojek <lukasz.rojek.mail@gmail.com>
2022-11-11 13:54:45 +0100

    changes on conf

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@ version = 'Education Edition'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = ['nbsphinx', 'sphinx.ext.autodoc']
+# extensions = ['nbsphinx', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
